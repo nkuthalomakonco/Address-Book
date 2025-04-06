3ae38a217b616d4d61b1666a2797330d8898adb0
nkuthalomakonco <nkuthalomakonco@gmail.com>
2025-04-06 21:41:38 +0200

    init

---
+++ main.py
@@ -0,0 +1,20 @@
+"""
+
+
+"""
+
+#imports 
+import random
+from datetime import datetime, timedelta
+
+
+def write(my_list,filename='sample.txt'):   
+    # Write to a file
+    pass
+
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
